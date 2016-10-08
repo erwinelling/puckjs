6af69d5d5d13866fd752aa19109d1b4a3a995d72
Erwin Elling <erwinelling@Erwins-MacBook-Pro.local>
2016-10-08 17:41:07 +0200

    switched to bluepy

---
+++ puck.py
@@ -0,0 +1,22 @@
+from bluepy import btle
+
+
+# TODO: Catch exception when Puck is not around
+conn = Peripheral("C3:25:1D:C7:EF:BD", btle.ADDR_TYPE_RANDOM)
+
+
+# for c in chars:
+# print c.uuid, c.getHandle(), c.propertiesToString(), c.read()
+
+
+ble_data = p.readCharacteristic(11)
+first_ble_datapoint = ble_data
+last_ble_datapoint = ble_data
+
+def transform_data(ble_data):
+    # transform
+    return ble_data
+
+while True:
+    print transform_data(p.readCharacteristic(11))
+    #TODO: maybe sleep for a short while here
