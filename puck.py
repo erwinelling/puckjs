83be285bc5c336040720019f6408a3f3f7c2443d
Erwin Elling <erwinelling@Erwins-MacBook-Pro.local>
2016-10-09 14:30:20 +0200

    cast data to int

--- puck.py
+++ puck.py
@@ -10,21 +10,23 @@ print "connected to %s" % (p)
 # print c.uuid, c.getHandle(), c.propertiesToString(), c.read()
 
 
-ble_datapoint = p.readCharacteristic(11)
+ble_datapoint = int(p.readCharacteristic(11))
 
 first_ble_datapoint = ble_datapoint
 last_ble_datapoint = ble_datapoint
 print "First datapoint: %s" % (first_ble_datapoint)
 
 def transform_data(ble_datapoint):
-    # afvangen als ik iets anders krijg dan 0-359
+
     # - het verschil tussen de nieuwe angle en oude omzetten naar een teller die iets van 500 graden is (iets minder dan twee keer de dop draaien zeg maar)
     # - afvangen wanneer hij van 0 naar 359 rolt zodat je geen rare effecten krijgt :]
     # - Die teller omzetten naar het volume getal (tussen de 0 en de 22)
     # - Dit uitsturen over netwerk in die ascii code die in het Bash script staat.
     global last_ble_datapoint
-    
+
+
     if ble_datapoint:
+        # TODO: afvangen als ik iets anders krijg dan integer 0-359
         previous_ble_datapoint = last_ble_datapoint
         last_ble_datapoint = ble_datapoint
         difference = ble_datapoint - last_ble_datapoint
@@ -38,7 +40,7 @@ def transform_data(ble_datapoint):
 
 try:
     while True:
-        transformed_data = transform_data(p.readCharacteristic(11))
+        transformed_data = transform_data(int(p.readCharacteristic(11)))
         if transformed_data:
             print "Transformed data: %s" % (transform_data)
         #TODO: maybe sleep for a shorter while here
